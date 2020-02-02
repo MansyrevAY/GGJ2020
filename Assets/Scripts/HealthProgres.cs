a4124512349edb34e52a8328d7121ae33df224a5
Arseny Mansyrev <mansyrevarsenij@gmail.com>
2020-02-02 12:37:38 +0300

    health and progress

---
+++ Assets/Scripts/HealthProgres.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthProgres : MonoBehaviour
+{
+    public BreachGenerator generator;
+    public Image hpBackground;
+
+    private int maxHp;
+    private int currentBreaches;
+    private int currentHP;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        maxHp = generator.maxNumberOnShip;
+        currentHP = maxHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentBreaches = BreachGenerator.CurrentNumberOnShip;
+        currentHP = maxHp - currentBreaches;
+
+        hpBackground.fillAmount = (float)currentHP / maxHp;
+    }
+}
